43473cda7b804b09ae4e15977269a8d29612e2d7
bessam-haj-salem <bessam-haj-salem@gmail.com>
2020-04-11 17:00:12 +0100

    ok

---
+++ jqueryFile.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+	// $('button#go').on('click', function(){ 
+	// 	$('p#output').text( $('input#color').val() ).css('color', 'red'); });
+
+
+	// $('button#go').dblclick(function(){
+	// 	alert('hello world');
+	// })
+
+    // $('div#secret').click(function(){
+    // 	$(this).hide();
+    // })
+	// $('body').css('background-color','rgb(153,0,0)');
+	// $('ol').append('<li> here list 4 </li>');
+// 	$(document).ready(function(){
+//   $("button").click(function(){
+//     $("#div1").hide();
+//     $("#div2").hide("slow");
+//     $("#div3").hide(3000);
+//   });
+// });
+	
+  $("#b1").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
+
+
+
